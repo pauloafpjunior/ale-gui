f590c6cdd68f532d46829a98c699a2ae9942abf2
Paulo Júnior <pauloafpjunior@hotmail.com>
2020-12-30 22:30:31 -0300

    feat: player inputs and movement (part 1)

---
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(-100)]
+public class PlayerInputs : MonoBehaviour
+{
+    public float horizontal { get; private set; }
+    public bool jump { get; private set; }
+
+    private bool readyToClear;
+
+    void FixedUpdate()
+    {
+        readyToClear = true;
+    }
+
+
+    void Update()
+    {
+        ClearInputs();
+        ProcessInputs();    
+    }
+
+    private void ClearInputs() {
+        if (!readyToClear) {
+            return;
+        }
+
+        horizontal = 0f;
+        jump = false;
+        readyToClear = false;
+    }
+
+    private void ProcessInputs() 
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        jump =  jump || Input.GetButtonDown("Jump");
+
+        print(horizontal + " - " + jump);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerInputs playerInputs;
+    private Rigidbody2D rb;
+    private float speed = 5;
+    private int direction = 1;
+
+    void Awake() 
+    {
+        playerInputs = GetComponent<PlayerInputs>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        GroundMovement();
+    }
+
+    private void GroundMovement() 
+    {
+        float xVelocity = speed * playerInputs.horizontal;
+
+        if (direction * xVelocity < 0) {
+            Flip();
+        }
+
+        rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+    }
+
+    private void Flip() 
+    {
+        direction *= -1;
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale; 
+    }
+
+}
